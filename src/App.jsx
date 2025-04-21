0458ac7eb16f9faf9ead08103f91c4d492c75430
tushar-kamra <tushar.kamra25@gmail.com>
2025-04-21 23:24:32 +0530

    basic ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import React from "react";
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+    );
+};
 
 export default App;
