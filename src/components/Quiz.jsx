d4b1f02d1ba1fda33fc3983d3f391e1f0b8698e6
tushar-kamra <tushar.kamra25@gmail.com>
2025-04-22 21:53:48 +0530

    shuffle options

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import QUESTIONS from "../../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
 
+    const quizCompleted = activeQuestionIndex === QUESTIONS.length;
+
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
     };
+
+    if (quizCompleted) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>
                                 {answer}
