bee7d80b684446632d7af1e4c1e7040294560ff7
tushar-kamra <tushar.kamra25@gmail.com>
2025-04-30 09:25:11 +0530

    highlight answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,40 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import QUESTIONS from "../../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
 
 const Quiz = () => {
+    const shuffledAnswers = useRef();
+    const [answerState, setAnswerState] = useState("");
     const [userAnswers, setUserAnswers] = useState([]);
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex =
+        answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
     const quizCompleted = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer = useCallback((selectedAnswer) => {
-        setUserAnswers((prevUserAnswers) => {
-            return [...prevUserAnswers, selectedAnswer];
-        });
-    }, []);
+    const handleSelectAnswer = useCallback(
+        (selectedAnswer) => {
+            setAnswerState("answered");
+            setUserAnswers((prevUserAnswers) => {
+                return [...prevUserAnswers, selectedAnswer];
+            });
+            setTimeout(() => {
+                if (
+                    selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]
+                ) {
+                    setAnswerState("correct");
+                } else {
+                    setAnswerState("wrong");
+                }
+
+                setTimeout(() => {
+                    setAnswerState("");
+                }, 2000);
+            }, 1000);
+        },
+        [activeQuestionIndex]
+    );
 
     const handleSkipAnswer = useCallback(
         () => handleSelectAnswer(null),
@@ -30,8 +50,10 @@ const Quiz = () => {
         );
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+        shuffledAnswers.current.sort(() => Math.random() - 0.5);
+    }
 
     return (
         <div id="quiz">
@@ -43,13 +65,32 @@ const Quiz = () => {
                 />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
-                                {answer}
-                            </button>
-                        </li>
-                    ))}
+                    {shuffledAnswers.current.map((answer) => {
+                        const isSelected =
+                            userAnswers[userAnswers.length - 1] === answer;
+                        let cssClasses = "";
+                        if (answerState === "answered" && isSelected) {
+                            cssClasses = "selected";
+                        }
+
+                        if (
+                            (answerState === "correct" ||
+                                answerState === "wrong") &&
+                            isSelected
+                        ) {
+                            cssClasses = answerState;
+                        }
+                        return (
+                            <li key={answer} className="answer">
+                                <button
+                                    onClick={() => handleSelectAnswer(answer)}
+                                    className={cssClasses}
+                                >
+                                    {answer}
+                                </button>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </div>
